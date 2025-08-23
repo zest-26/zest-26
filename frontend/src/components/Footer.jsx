273ae6697cd1755f8ee7ba0b07a3fec07cc51db2
Palak K <palakkongale@gmail.com>
2025-08-24 00:42:04 +0530

    done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Phone,Mail,MapPin,Linkedin,Github,Instagram, Twitter,Facebook} from 'lucide-react'
+import {Phone,Mail,MapPin,Linkedin,Github,Instagram, Twitter,Facebook,X} from 'lucide-react'
 
 
 const Footer = () => {
@@ -10,11 +10,15 @@ const Footer = () => {
 </h1>
   <div className='flex relative justify-evenly ml-[60px] mr-[60px] sm:px-15 sm:mt-6 sm:ml-150 sm:mr-150'>
               <a href='https://www.instagram.com/coepzest/' target="_blank" 
-        rel="noopener noreferrer"> <Instagram color="currentColor" size={30} /></a>
+        rel="noopener noreferrer"> <Instagram color="white" size={30} /></a>
               <a href='https://www.facebook.com/people/Coep-Zest/61571312685575/?mibextid=wwXIfr&rdid=DH1ZYqMPnrG976dm&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F16dd5rFCFF%2F%3Fmibextid%3DwwXIfr' target="_blank" 
-        rel="noopener noreferrer"><Facebook color="currentColor" size={30} /> </a>
+        rel="noopener noreferrer"><Facebook color="white" size={30} /> </a>
+        <a href='https://www.linkedin.com/company/zest-coep/?originalSubdomain=in' target="_blank" 
+        rel="noopener noreferrer"> <Linkedin color="white" size={30} /></a>
+        <a href='https://x.com/zest_coep' target="_blank" 
+        rel="noopener noreferrer"> <X color="white" size={30} /></a>
             </div>
-         <p className='sm:mt-10 mt-10 ml-[50px] sm:ml-[640px] absolute '>© 2025 ZEST'26. All rights reserved.</p>
+         <p className='sm:mt-10 mt-10 ml-[50px] sm:ml-[640px] text-white absolute '>© 2025 ZEST'26. All rights reserved.</p>
     </div>
   )
 }
--- frontend/src/pages/coordinatorResults.jsx
+++ frontend/src/pages/coordinatorResults.jsx
@@ -22,36 +22,54 @@ const CoordinatorResults = () => {
    
   }, []);
 
+  useEffect(() => {
+    gsap.to(".football", {
+    y: -30,
+    duration: 1.3,
+    repeat: -1,
+    yoyo: true,
+    ease: "sine.inOut",
+  });
+ 
+
+  }, []);
+
   return (
-    <div className=" relative h-[750px] sm:h-[1090px] flex items-center justify-center bg-gradient-to-b from-black to-[#3a1802]">
-      <div className="absolute hidden sm:block h-[930px] bg-gradient-to-b from-[#b94d05] to-black w-full mt-[-980px]"></div>
+    <div className=" relative h-[700px] sm:h-[1090px] flex items-center justify-center bg-gradient-to-b from-black to-[#361701]">
+      <div className="absolute h-[950px] sm:h-[930px] bg-gradient-to-b from-[#b94d05] to-black w-full mt-[-900px] sm:mt-[-980px]"></div>
+      
+      <img src='/football.png' className="football brightness-90 absolute h-[100px] w-[100px] top-[18px] left-[-20px] sm:h-[300px] sm:w-[300px] sm:top-[-40px] sm:left-[-40px]"/>
+      <img src='/football.png' className="football brightness-90 absolute h-[100px] w-[100px] top-[18px] right-[-20px] sm:h-[300px] sm:w-[300px] sm:top-[-40px] sm:right-[-40px]"/>
+      <img src='/football.png' className="football brightness-90 hidden sm:block absolute  sm:h-[300px] sm:w-[300px] sm:top-[790px] sm:right-[-40px]"/>
+      <img src='/football.png' className="football brightness-90 hidden sm:block absolute  sm:h-[300px] sm:w-[300px] sm:top-[790px] sm:left-[-40px]"/>
+
        {/* Logo at top */}
           <div className="absolute top-[27px] w-[230px] h-[70px] sm:w-[270px] sm:h-[100px] overflow-hidden z-10">
             <img src="/ZEST-26.png" alt="Logo" className="w-full h-full object-contain" />
           </div>
 
-          <h1 className="absolute top-[120px]   sm:top-[155px] text-2xl  sm:text-4xl font-bold " ref={textRef}>
+          <h1 className="absolute top-[120px] sm:top-[155px] text-2xl  sm:text-4xl font-bold " ref={textRef}>
             {"Coordinator Results".split("").map((char, index) => (
           <span
             key={index}
-            className="inline-block opacity-0 translate-y-[-20px]"
+            className="inline-block text-white opacity-0 translate-y-[-20px]"
           >
             {char === " " ? "\u00A0" : char}
           </span>
         ))}</h1>
 
       <div
-        className="w-full h-[720px] sm:w-[660px] sm:h-[790px] top-[200px] bg-cover pb-[20px]  bg-center absolute rounded-2xl  shadow-lg overflow-y-auto scrollbar-none"
+        className="w-full h-[520px] sm:w-[660px] sm:h-[790px] top-[180px] sm:top-[200px] bg-cover pb-[20px]  bg-center absolute rounded-2xl  shadow-lg overflow-y-auto scrollbar-none"
         style={{ backgroundImage: "url('/bg-6.png')" }} // put bg2.avif in /public folder
       >
         {/* Dark overlay */}
-  <div className="absolute inset-0 bg-black/40 h-[1400px] rounded-2xl z-0"></div>
+  <div className="absolute inset-0 bg-black/40 h-[1270px] sm:h-[1400px] rounded-2xl z-0"></div>
 
 
         {/* Content goes on top of image */}
         <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
           
-          <div className="absolute mr-10 sm:mr-13 sm:top-[30px] sm:pb-[100px]  top-[345px] w-[400px]  rounded-2xl   shadow-md">
+          <div className="absolute mr-10 sm:mr-13 sm:top-[30px]   sm:pb-[100px] top-[165px] w-[400px]  rounded-2xl   shadow-md">
             <Tabs />
           </div>
           
