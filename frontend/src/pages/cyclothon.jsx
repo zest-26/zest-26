552c8f872b33a186c02d2289570f8f7c418094be
Palak K <palakkongale@gmail.com>
2025-09-18 20:33:10 +0530

    cycle madel is positioned

--- frontend/src/pages/cyclothon.jsx
+++ frontend/src/pages/cyclothon.jsx
@@ -9,14 +9,14 @@ function Model() {
   }
 });
 
-   return <primitive object={scene} scale={2} rotation={[Math.PI, 0, 0]} />;
+   return <primitive object={scene} scale={0.2} position={[4, -10, 0]} rotation={[Math.PI, 1.3, 0]} />;
 }
 
 export default function cyclothon() {
   return (
     <div className="h-screen w-screen relative bg-[#b94d05]">
       <div className="h-[550px] w-[550px] absolute mt-[350px] ml-[500px]">
-      <Canvas camera={{ position: [0, 2, 5], fov: 50 }}>
+      <Canvas camera={{ position: [0, 30, 50], fov: 50 }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} />
         <Model />
