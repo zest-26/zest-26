3c93a4abfb038d8319490c08995b6fc3057d973c
Palak K <palakkongale@gmail.com>
2025-09-18 20:08:04 +0530

    cycle set to a position

--- frontend/src/pages/cyclothon.jsx
+++ frontend/src/pages/cyclothon.jsx
@@ -14,7 +14,8 @@ function Model() {
 
 export default function cyclothon() {
   return (
-    <div className="h-screen w-screen bg-[#b94d05]">
+    <div className="h-screen w-screen relative bg-[#b94d05]">
+      <div className="h-[550px] w-[550px] absolute mt-[350px] ml-[500px]">
       <Canvas camera={{ position: [0, 2, 5], fov: 50 }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} />
@@ -27,7 +28,7 @@ export default function cyclothon() {
 />
 
       </Canvas>
+      </div>
     </div>
   );
 }
-
