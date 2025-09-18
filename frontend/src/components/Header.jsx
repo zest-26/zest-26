4280f512e67036c39a23122de8437cf1547c5f83
Palak K <palakkongale@gmail.com>
2025-09-18 16:28:44 +0530

    header removed from other pages

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <nav className="fixed top-0 w-full h-20 font-bold text-[18px]  bg-white/10 backdrop-blur-md shadow-md z-50 pt-3">
       <ul className="relative flex justify-evenly space-x-8 p-4 bg-gradient-to-r from-purple-200 to-blue-200 text-transparent bg-clip-text ">
         <li><Link to="/" className=''><img src='./ZEST-26.png' className='w-[100px] h-[40px] mb-1 absolute left-[13px] top-[7px]'/></Link></li>
-        <li><Link to="#cyclothon" className='text-orange-600 hover:text-white transition-colors duration-200'>Cyclothon</Link></li>
+        <li><Link to="cyclothon" className='text-orange-600 hover:text-white transition-colors duration-200'>Cyclothon</Link></li>
         <li><a href="#marathon" className='text-orange-600 hover:text-white transition-colors duration-200'>Marathon</a></li>
         <li><Link to="Gallery" className='text-orange-600 hover:text-white transition-colors duration-200'>Gallery</Link></li>
         <li><Link to="coreTeam" className='text-orange-600 hover:text-white transition-colors duration-200'>Core Team</Link></li>
